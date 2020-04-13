90a393a1e09f2cd7aebcd0c4f1f015c69623453d
pojoba23 <62113047+pojoba23@users.noreply.github.com>
2020-04-13 10:53:13 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
-// This form can not be left blank.
+// This function make sure that the form can not be left blank.
 function isValidName (name) {
-    return /[a-z]+/.test(name);
+
+}
+
+//this function makes sure that the form must be formatted as an email address, not a valid one just formated
+function isValidEmail (email){
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+
+}
+
+//this function makes the user select at least one checkbox under "Register for Activies" section
+function isvalidActivity (Activies) {
+
+
+}
+
+//this function makes the user enter a Credit Card Number, a Zip Code and a 3 number CVV if Credit Card is selected
+function isValidCC (creditCard){
+
 
 }
