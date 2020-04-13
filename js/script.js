6729c2ac93913dd6cd4219e64a26f229cd95a2eb
pojoba23 <62113047+pojoba23@users.noreply.github.com>
2020-04-13 10:34:26 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,26 +1,5 @@
-'use strict';
+// This form can not be left blank.
+function isValidName (name) {
+    return /[a-z]+/.test(name);
 
-var jobRoleSelect = document.getElementById('title');
-var basicSection = document.getElementsByClassName('basic')[0];
-
-document.getElementById("title").addEventListener("change", function(){
-  var jobSelected = jobRoleSelect.options[jobRoleSelect.selectedIndex].value;
-  console.log(jobSelected);
-  
-  if(jobSelected === 'other') {
-    var otherText = document.createElement('input');
-    // Add an text input field. Use the id of "other-title" 
-    
-    otherText.setAttribute('id', 'other-title');
-    otherText.setAttribute('type', 'text');
-    otherText.setAttribute('name', 'other_field');
-    otherText.setAttribute('placeholder', 'Your Title');
-    
-    var otherLabel = document.createElement('label');
-    otherLabel.setAttribute('for', 'other_field');
-    otherLabel.innerHTML = 'Other:';
-
-    basicSection.appendChild(otherLabel);
-    basicSection.appendChild(otherText);
-  }
-});
+}
